139fdc62168f8f4c5b306d941fc261ca6304267c
dragan1810 <d.novakovic1810@yahoo.com>
2020-09-28 21:49:06 +0200

    qqq

--- lfg_front/lib/src/pages/login.dart
+++ lfg_front/lib/src/pages/login.dart
@@ -20,6 +20,20 @@ class _LoginPageState extends State<LoginPage> {
                       MediaQuery.of(context).size.width * 0.3,
                       0,
                       MediaQuery.of(context).size.width * 0.3,
+                      10.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderSide: new BorderSide(color: Colors.teal),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      MediaQuery.of(context).size.width * 0.3,
+                      0,
+                      MediaQuery.of(context).size.width * 0.3,
                       0),
                   child: TextField(
                     decoration: InputDecoration(
@@ -50,14 +64,32 @@ class _LoginPageState extends State<LoginPage> {
                           height: 48.0,
                         ),
                         Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            border: Border(
+                              top: BorderSide(width: 1.0, color: Colors.grey),
+                              left: BorderSide(width: 1.0, color: Colors.grey),
+                              right: BorderSide(width: 1.0, color: Colors.grey),
+                              bottom:
+                                  BorderSide(width: 1.0, color: Colors.grey),
+                            ),
+                          ),
                           margin: EdgeInsets.all(10.0),
-                          color: Colors.amber[600],
                           width: 48.0,
                           height: 48.0,
                         ),
                         Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            border: Border(
+                              top: BorderSide(width: 1.0, color: Colors.grey),
+                              left: BorderSide(width: 1.0, color: Colors.grey),
+                              right: BorderSide(width: 1.0, color: Colors.grey),
+                              bottom:
+                                  BorderSide(width: 1.0, color: Colors.grey),
+                            ),
+                          ),
                           margin: EdgeInsets.all(10.0),
-                          color: Colors.amber[600],
                           width: 48.0,
                           height: 48.0,
                         ),
